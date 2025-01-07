029323cda3136ee20179ce343719ce763ca4f41e
Thurcavalcante <cavalcantethur@gmail.com>
2025-01-07 15:34:55 -0300

    docs(Mudanda nos arquivos) Alteração do valor de idade, altura e estado civil

--- Estudos-em-C/03-Tipos-de-Dados/main.c
+++ Estudos-em-C/03-Tipos-de-Dados/main.c
@@ -2,18 +2,18 @@
 
 int main(void) {
 // Declara uma variável inteira chamada "idade" e atribui o valor 25
-int idade = 25;
+int idade = 26;
 
 // Declara uma variável de ponto flutuante chamada "altura" e atribui o valor 1.80
-float altura = 1.80;
+float altura = 1.72;
 
 // Declara uma variável de caractere chamada "sexo" e atribui o valor 'M'
 char sexo = 'M';
 
 // Declara uma variável booleana chamada "casado" e atribui o valor verdadeiro
-_Bool casado = 1;
+_Bool namorando = 1;
 
-    printf("Idade: %d\nAltura: %f\nSexo: %c\nCasado: %d\n", idade, altura, sexo, casado);
+    printf("Idade: %d\nAltura: %f\nSexo: %c\nNamorando: %d\n", idade, altura, sexo, namorando);
 
     return 0;
 }
